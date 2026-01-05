63a114c299f3f99de5faa6255fb914f81e6af5b3
Zecho <faiz58843@gmail.com>
2026-01-05 22:16:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,6 +126,7 @@ if [ "$SELECT_THEME" -eq 1 ]; then
   sudo cp -rfT /root/pterodactyl /var/www/pterodactyl
   curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
   sudo apt install -y nodejs
+  sudo apt install npm
   sudo npm i -g yarn
   cd /var/www/pterodactyl
   yarn add react-feather
